bcde66dee2045fd5dde22fb2a09ce47372f28025
ihatov08 <ihatov08@gmail.com>
2018-11-22 01:41:15 +0900

    Implement upload image controller

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,4 +1,12 @@
 class UploadController < ApplicationController
   def image
+    file = params[:asset][:file]
+    blob = ActiveStorage::Blob.create_after_upload!(
+      io: file,
+      filename: file.original_filename,
+      content_type: file.content_type
+    )
+
+    render json: { filename: url_for(blob) }
   end
 end
